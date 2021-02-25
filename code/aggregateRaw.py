c64a74e3fbeffaa2ce4f22f7ebbdc568541bd339
adamConnerSax <adam_conner_sax@yahoo.com>
2021-02-25 12:24:56 -0500

    Added dasymetric and 2014 and 2012.

--- code/aggregateRaw.py
+++ code/aggregateRaw.py
@@ -2,21 +2,155 @@ import geopandas
 import pandas
 import tobler
 import re
+from quilt3 import Package
 geopandas.options.use_pygeos = True
 
 # Some notes
-# All but citizenshep available from census at block-group level
-# Citizenship available from 5-year ACS at Tract level
-# So we build each, Use areal weighting to aggregate to districts and then merge
+# 5-year ACS has much at the tract level
+# Comes in 2 sets so code below can merge those
 
-print("Loading block group data tables")
+# 2018
+dataCSVs = ["input_data/NHGIS/US_2018_tract_csv/nhgis0016_ds240_20185_2018_tract_E.csv"]
+dataShapes = "input_data/NHGIS/US_2018_tract_shapefile/US_tract_2018.shp"
+aggregateToShape = "input_data/CongressionalDistricts/cd116/tl_2018_us_cd116.shp"
+outCSV = "output_data/US_2018_cd116/cd116Raw.csv"
+cdCol = 'CD116FP'
+nlcd = "nlcd_2011.tif"
+
+#2016
+#dataCSVs = ["input_data/NHGIS/US_2016_tract_csv/nhgis0015_ds226_20165_2016_tract_E.csv"]
+#dataShapes = "input_data/NHGIS/US_2016_tract_shapefile/US_tract_2016.shp"
+#aggregateToShape = "input_data/CongressionalDistricts/cd115/tl_2016_us_cd115.shp"
+#outCSV = "output_data/US_2016_cd115/cd115Raw.csv"
+#cdCol = 'CD115FP'
+#nlcd = "nlcd_2016.tif"
+
+#2014
+#dataCSVs = ["input_data/NHGIS/US_2014_tract_csv/nhgis0017_ds207_20145_2014_tract_E.csv"]
+#dataShapes = "input_data/NHGIS/US_2014_tract_shapefile/US_tract_2014.shp"
+#aggregateToShape = "input_data/CongressionalDistricts/cd114/tl_2014_us_cd114.shp"
+#outCSV = "output_data/US_2014_cd114/cd114Raw.csv"
+#cdCol = 'CD114FP'
+#nlcd = "nlcd_2011.tif"
+
+#2012
+#dataCSVs = ["input_data/NHGIS/US_2014_tract_csv/nhgis0018_ds192_20125_2012_tract_E.csv"]
+#dataShapes = "input_data/NHGIS/US_2012_tract_shapefile/US_tract_2012.shp"
+#aggregateToShape = "input_data/CongressionalDistricts/cd113/tl_2012_us_cd113.shp"
+#outCSV = "output_data/US_2012_cd113/cd113Raw.csv"
+#cdCol = 'CD113FP'
+#nlcd = "nlcd_2011.tif"
+
+
+
+
 
 def dataAndColsForMerge(fp):
+    print("Loading tract data from ", fp)
     pat = re.compile('^[A-Z0-9]+E\d+$')
     df = pandas.read_csv(fp, encoding='latin-1')
     dataCols = [s for s in df.columns if pat.match(s)]
     colVals = ['GISJOIN'] + dataCols
     return df.filter(colVals, axis=1), dataCols
+
+def loadAndJoinData(fps, joinCol='GISJOIN'):
+    if len(fps) == 0:
+        raise RuntimeError("No files given to loadAndJoinData")
+    print("Loading tract data tables")
+    fp0 = fps.pop()
+    df, dataCols = dataAndColsForMerge(fp0)
+    for fp in fps:
+        dfA, dataColsA = dataAndColsForMerge(fp)
+        print ("joining data on", joinCol)
+        df.set_index(joinCol).join(dfA.set_index(joinCol))
+        dataCols = dataCols + dataColsA
+    return df, dataCols
+
+
+def loadShapesAndData(dataFPS, shapeFP, joinCol='GISJOIN'):
+    df_dat, dataCols = loadAndJoinData(dataFPS, joinCol)
+    print (df_dat.head())
+    print("Loading tract shapefile")
+    df_geo = geopandas.read_file(shapeFP)
+    print(df_geo.head())
+    print("Merging data into shapes")
+    df_geo = df_geo.merge(df_dat, on=joinCol)
+    return df_geo, dataCols
+
+def loadAggregateToShapes(fp):
+    print("Loading aggregate-to shapefile")
+    agg_geo = geopandas.read_file(fp)
+    print ("Adding areas, after projecting to CEA")
+    agg_geo = agg_geo.to_crs({'proj':'cea'})
+    print ("Computing areas")
+    sq_meters_per_sq_mile = 1609.34 * 1609.34
+    sq_meters_per_sq_km = 1e6
+    agg_geo["SqMiles"] = agg_geo['geometry'].area / sq_meters_per_sq_mile
+    agg_geo["SqKm"] = agg_geo['geometry'].area / sq_meters_per_sq_km
+    return agg_geo
+
+def reformat(x):
+    if (isinstance(x, float)):
+        return '{:.0f}'.format(x)
+    else:
+        return x
+
+def reProjectBoth(df_dat, df_agg, crs='EPSG:3857'):
+    print("Projecting small areas to ", crs)
+    df_dat = df_dat.to_crs(crs)
+    print("Projecting aggregate-to areas to ",crs)
+    df_agg = df_agg.to_crs(crs)
+    return df_dat, df_agg
+
+def aggregate_simple(df_dat, df_agg, dataCols, districtFIPSCol, stateFIPSCol='STATEFP', nJobs=-1):
+    crs = "EPSG:3857"
+    df_dat, df_agg = reProjectBoth(df_dat, df_agg, crs)
+    print("Aggregating small areas (via areal interpolation)")
+    df_interp = tobler.area_weighted.area_interpolate(df_dat, df_agg, extensive_variables=dataCols, n_jobs=nJobs)
+    df_interp = pandas.concat([df_agg[[stateFIPSCol, districtFIPSCol] + ['SqMiles','SqKm']], df_interp],axis=1) # put the keys + areas back
+    print("Removing ZZ entries")
+    df_interp = df_interp[(df_interp[districtFIPSCol] != "ZZ")]
+    df_interp = df_interp.rename(columns={stateFIPSCol: "StateFIPS", districtFIPSCol: "CongressionalDistrict"})
+    print ("Reformatting numbers...")
+    for col in dataCols:
+        df_interp[col] = df_interp[col].map(lambda x: reformat(x))
+    df_interp["SqMiles"] = df_interp["SqMiles"].map(lambda x: '{:.2f}'.format(x))
+    df_interp["SqKm"] = df_interp["SqKm"].map(lambda x: '{:.2f}'.format(x))
+    return df_interp
+
+def aggregate_dasymmetric(nlcd, df_dat, df_agg, dataCols, districtFIPSCol, stateFIPSCol='STATEFP', nJobs=-1):
+    crs = "EPSG:4326"
+    df_dat, df_agg = reProjectBoth(df_dat, df_agg, crs)
+    print("loading nlcd raster data via quilt")
+    p = Package.browse("rasters/nlcd", "s3://spatial-ucr")
+    p[nlcd].fetch()
+    print("Aggregating small areas (via dasymetric areal interpolation using NLCD raster data)")
+    df_interp = tobler.dasymetric.masked_area_interpolate(raster=nlcd
+                                                          , source_df=df_dat
+                                                          , target_df=df_agg
+                                                          , extensive_variables=dataCols)
+    df_interp = pandas.concat([df_agg[[stateFIPSCol, districtFIPSCol] + ['SqMiles','SqKm']], df_interp],axis=1) # put the keys + areas back
+    print("Removing ZZ entries")
+    df_interp = df_interp[(df_interp[districtFIPSCol] != "ZZ")]
+    df_interp = df_interp.rename(columns={stateFIPSCol: "StateFIPS", districtFIPSCol: "CongressionalDistrict"})
+    print ("Reformatting numbers...")
+    for col in dataCols:
+        df_interp[col] = df_interp[col].map(lambda x: reformat(x))
+    df_interp["SqMiles"] = df_interp["SqMiles"].map(lambda x: '{:.2f}'.format(x))
+    df_interp["SqKm"] = df_interp["SqKm"].map(lambda x: '{:.2f}'.format(x))
+    return df_interp
+
+
+df_tracts, tract_dataCols = loadShapesAndData(dataCSVs, dataShapes)
+df_cds = loadAggregateToShapes(aggregateToShape)
+df_aggregated = aggregate_dasymmetric(nlcd, df_tracts, df_cds, tract_dataCols, cdCol)
+
+outCols = ['StateFIPS','CongressionalDistrict','SqMiles','SqKm'] + tract_dataCols
+
+print ("Writing ", outCSV)
+df_aggregated[outCols].to_csv(outCSV, index=False)
+print ("done.")
+
 
 
 #bg_dataTable, bg_dataCols = dataAndColsForMerge("input_data/NHGIS/US_2010_blk_grp_csv/nhgis0003_ds176_20105_2010_blck_grp.csv")
@@ -31,79 +165,3 @@ def dataAndColsForMerge(fp):
 #print("Merging block group data into block group shapefile")
 #blk_grp_geo = blk_grp_geo.merge(bg_dataTable, on='GISJOIN')
 #print(blk_grp_geo.head())
-
-print("Loading tract data tables")
-tractA_df, tractACols = dataAndColsForMerge("input_data/NHGIS/US_2018_tract_csv/nhgis0006_ds239_20185_2018_tract_E.csv")
-tractB_df, tractBCols = dataAndColsForMerge("input_data/NHGIS/US_2018_tract_csv/nhgis0006_ds240_20185_2018_tract_E.csv")
-
-tract_dataCols = tractACols + tractBCols
-
-print("Joining tract-level data")
-tract_df = tractA_df.set_index('GISJOIN').join(tractB_df.set_index('GISJOIN'))
-print(tract_df.head())
-
-print("Loading tract shapefile")
-tract_geo = geopandas.read_file("input_data/NHGIS/US_2018_tract_shapefile/US_tract_2018.shp")
-print(tract_geo.head())
-print(tract_geo.crs)
-
-print("Merging tract-level-data into tract shapefile")
-tract_geo = tract_geo.merge(tract_df, on='GISJOIN')
-
-print("Loading congressional district shapefile")
-cd_geo = geopandas.read_file("input_data/CongressionalDistricts/cd116/tl_2018_us_cd116.shp")
-
-# There are entries with CD116FP == 'ZZ' representing census areas with no people.  Leave them in until later!
-# Otherwise tobler gets confused somehow
-
-print ("Adding areas to districts")
-print ("Re-projecting to CEA")
-cd_geo = cd_geo.to_crs({'proj':'cea'})
-print ("Computing areas")
-sq_meters_per_sq_mile = 1609.34 * 1609.34
-sq_meters_per_sq_km = 1e6
-cd_geo["SqMiles"] = cd_geo['geometry'].area / sq_meters_per_sq_mile
-cd_geo["SqKm"] = cd_geo['geometry'].area / sq_meters_per_sq_km
-
-crs = "EPSG:3857"
-#print("Projecting block groups to ", crs)
-#blk_grp_geo = blk_grp_geo.to_crs(crs)
-print("Projecting tracts to ", crs)
-tract_geo = tract_geo.to_crs(crs)
-print("Projecting districts to ",crs)
-cd_geo = cd_geo.to_crs(crs)
-
-
-#print("Aggregating block group data into districts (via areal interpolation)")
-#cd_bg_interp = tobler.area_weighted.area_interpolate(blk_grp_geo, cd_geo, extensive_variables=bg_dataCols, n_jobs=-1)
-#cd_bg_interp_rekeyed = pandas.concat([cd_geo[['STATEFP','CD116FP','SqMiles','SqKm']], cd_bg_interp],axis=1) # put the keys + areas back
-
-print("Aggregating tract data into districts (via areal interpolation)")
-cd_tract_interp = tobler.area_weighted.area_interpolate(tract_geo, cd_geo, extensive_variables=tract_dataCols, n_jobs=-1)
-cd_tract_interp_rekeyed = pandas.concat([cd_geo[['STATEFP','CD116FP','SqMiles','SqKm']], cd_tract_interp],axis=1) # put the keys + areas back
-cd_interp = cd_tract_interp_rekeyed #cd_bg_interp_rekeyed.join(cd_tract_interp[tract_dataCols],rsuffix="_tract")
-
-print("Removing ZZ entries")
-cd_interp = cd_interp[(cd_interp.CD116FP != "ZZ")]
-
-cd_interp = cd_interp.rename(columns={"STATEFP": "StateFIPS", "CD116FP": "CongressionalDistrict"})
-
-outCols = ['StateFIPS','CongressionalDistrict','SqMiles','SqKm'] + tract_dataCols
-
-print ("Reformatting numbers...")
-
-def reformat(x):
-    if (isinstance(x, float)):
-        return '{:.0f}'.format(x)
-    else:
-        return x
-
-for col in tract_dataCols:
-    cd_interp[col] = cd_interp[col].map(lambda x: reformat(x))
-
-cd_interp["SqMiles"] = cd_interp["SqMiles"].map(lambda x: '{:.2f}'.format(x))
-cd_interp["SqKm"] = cd_interp["SqKm"].map(lambda x: '{:.2f}'.format(x))
-
-print ("Saving CSV")
-cd_interp[outCols].to_csv("output_data/US_2018_cd116/cd116Raw.csv", index=False) # , float_format="%.1f")
-print ("done.")
