90d01a9fad0cdf53ee2cd64367c6fccb706c684b
adamConnerSax <adam_conner_sax@yahoo.com>
2021-02-28 15:52:42 -0500

    Added PerCapitaIncome and density

--- code/aggregateRaw.py
+++ code/aggregateRaw.py
@@ -10,40 +10,51 @@ geopandas.options.use_pygeos = True
 # Comes in 2 sets so code below can merge those
 
 # 2018
-dataCSVs = ["input_data/NHGIS/US_2018_tract_csv/nhgis0016_ds240_20185_2018_tract_E.csv"]
-dataShapes = "input_data/NHGIS/US_2018_tract_shapefile/US_tract_2018.shp"
-aggregateToShape = "input_data/CongressionalDistricts/cd116/tl_2018_us_cd116.shp"
-outCSV = "output_data/US_2018_cd116/cd116Raw.csv"
-cdCol = 'CD116FP'
-nlcd = "nlcd_2011.tif"
+#dataCSVs = ["input_data/NHGIS/US_2018_tract_csv/nhgis0016_ds240_20185_2018_tract_E.csv"
+#            , "input_data/NHGIS/US_2018_tract_csv/nhgis0022_ds239_20185_2018_tract_E.csv"]
+#dataShapes = "input_data/NHGIS/US_2018_tract_shapefile/US_tract_2018.shp"
+#aggregateToShape = "input_data/CongressionalDistricts/cd116/tl_2018_us_cd116.shp"
+#outCSV = "output_data/US_2018_cd116/cd116Raw.csv"
+#cdCol = 'CD116FP'
+#totalPopCol = 'AJWME001'
+#pcIncomeCol = 'AJ0EE001'
+#nlcd = "nlcd_2011.tif"
 
 #2016
-#dataCSVs = ["input_data/NHGIS/US_2016_tract_csv/nhgis0015_ds226_20165_2016_tract_E.csv"]
+#dataCSVs = ["input_data/NHGIS/US_2016_tract_csv/nhgis0015_ds226_20165_2016_tract_E.csv"
+#            , "input_data/NHGIS/US_2016_tract_csv/nhgis0023_ds225_20165_2016_tract_E.csv"]
 #dataShapes = "input_data/NHGIS/US_2016_tract_shapefile/US_tract_2016.shp"
 #aggregateToShape = "input_data/CongressionalDistricts/cd115/tl_2016_us_cd115.shp"
 #outCSV = "output_data/US_2016_cd115/cd115Raw.csv"
 #cdCol = 'CD115FP'
+#totalPopCol = 'AF2LE001'
+#pcIncomeCol = 'AF6AE001'
 #nlcd = "nlcd_2016.tif"
 
 #2014
-#dataCSVs = ["input_data/NHGIS/US_2014_tract_csv/nhgis0017_ds207_20145_2014_tract_E.csv"]
+#dataCSVs = ["input_data/NHGIS/US_2014_tract_csv/nhgis0017_ds207_20145_2014_tract_E.csv"
+#            , "input_data/NHGIS/US_2014_tract_csv/nhgis0024_ds206_20145_2014_tract_E.csv"]
 #dataShapes = "input_data/NHGIS/US_2014_tract_shapefile/US_tract_2014.shp"
 #aggregateToShape = "input_data/CongressionalDistricts/cd114/tl_2014_us_cd114.shp"
 #outCSV = "output_data/US_2014_cd114/cd114Raw.csv"
 #cdCol = 'CD114FP'
+#totalPopCol = 'ABA1E001'
+#pcIncomeCol = 'ABFIE001'
 #nlcd = "nlcd_2011.tif"
 
 #2012
-#dataCSVs = ["input_data/NHGIS/US_2012_tract_csv/nhgis0018_ds192_20125_2012_tract_E.csv"]
-#dataShapes = "input_data/NHGIS/US_2012_tract_shapefile/US_tract_2012.shp"
-#aggregateToShape = "input_data/CongressionalDistricts/cd113/tl_2013_us_cd113.shp" # this is weird, the 2013 bit
-#outCSV = "output_data/US_2012_cd113/cd113Raw.csv"
-#cdCol = 'CD113FP'
+dataCSVs = ["input_data/NHGIS/US_2012_tract_csv/nhgis0018_ds192_20125_2012_tract_E.csv"
+           , "input_data/NHGIS/US_2012_tract_csv/nhgis0026_ds191_20125_2012_tract_E.csv"]
+dataShapes = "input_data/NHGIS/US_2012_tract_shapefile/US_tract_2012.shp"
+aggregateToShape = "input_data/CongressionalDistricts/cd113/tl_2013_us_cd113.shp" # this is weird, the 2013 bit
+outCSV = "output_data/US_2012_cd113/cd113Raw.csv"
+cdCol = 'CD113FP'
+totalPopCol = 'QSPE001'
+pcIncomeCol = 'QWUE001'
 #nlcd = "nlcd_2011.tif"
 
-
-
-
+extraIntCols =['TotalPopulation']
+extraFloatCols = ['PerCapitaIncome','SqKm','SqMiles','PopPerSqMile','pwPopPerSqMile','SqKmPop']
 
 def dataAndColsForMerge(fp):
     print("Loading tract data from ", fp)
@@ -62,19 +73,29 @@ def loadAndJoinData(fps, joinCol='GISJOIN'):
     for fp in fps:
         dfA, dataColsA = dataAndColsForMerge(fp)
         print ("joining data on", joinCol)
-        df.set_index(joinCol).join(dfA.set_index(joinCol))
+        df = df.set_index(joinCol).join(dfA.set_index(joinCol))
         dataCols = dataCols + dataColsA
     return df, dataCols
 
+def addPopAndIncome(df_dat, popC, pcIncomeC):
+    df_dat["TotalPopulation"] = df_dat[popC]
+    df_dat["TotalIncome"] = df_dat[popC] * df_dat[pcIncomeC]
+    return df_dat
 
-def loadShapesAndData(dataFPS, shapeFP, joinCol='GISJOIN'):
+def loadShapesAndData(dataFPS, shapeFP, popC, pcIncomeC, joinCol='GISJOIN'):
     df_dat, dataCols = loadAndJoinData(dataFPS, joinCol)
+    df_dat = addPopAndIncome(df_dat, popC, pcIncomeC)
     print (df_dat.head())
     print("Loading tract shapefile")
     df_geo = geopandas.read_file(shapeFP)
+    print ("Adding area*pop, after projecting to CEA")
+    df_geo = df_geo.to_crs({'proj':'cea'})
     print(df_geo.head())
     print("Merging data into shapes")
     df_geo = df_geo.merge(df_dat, on=joinCol)
+    print ("Adding (CEA) area-weighted pop")
+    sq_meters_per_sq_km = 1e6
+    df_geo["SqKmPop"] = df_geo["TotalPopulation"] * df_geo['geometry'].area / sq_meters_per_sq_km
     return df_geo, dataCols
 
 def loadAggregateToShapes(fp):
@@ -106,16 +127,23 @@ def aggregate_simple(df_dat, df_agg, dataCols, districtFIPSCol, stateFIPSCol='ST
     crs = 'EPSG:3857'
     df_dat, df_agg = reProjectBoth(df_dat, df_agg, crs)
     print("Aggregating small areas (via areal interpolation)")
-    df_interp = tobler.area_weighted.area_interpolate(df_dat, df_agg, extensive_variables=dataCols, n_jobs=nJobs)
+    df_interp = tobler.area_weighted.area_interpolate(df_dat
+                                                      , df_agg
+                                                      , extensive_variables=(['TotalPopulation', 'TotalIncome','SqKmPop'] + dataCols)
+                                                      , n_jobs=nJobs)
     df_interp = pandas.concat([df_agg[[stateFIPSCol, districtFIPSCol] + ['SqMiles','SqKm']], df_interp],axis=1) # put the keys + areas back
     print("Removing ZZ entries")
     df_interp = df_interp[(df_interp[districtFIPSCol] != "ZZ")]
     df_interp = df_interp.rename(columns={stateFIPSCol: "StateFIPS", districtFIPSCol: "CongressionalDistrict"})
+    df_interp["PerCapitaIncome"] = df_interp["TotalIncome"]/df_interp["TotalPopulation"]
+    df_interp["PopPerSqMile"] = df_interp["TotalPopulation"]/df_interp["SqMiles"]
+    sqKm_per_sqMi = 2.58999
+    df_interp["pwPopPerSqMile"] = (df_interp["TotalPopulation"] * df_interp["TotalPopulation"]/df_interp["SqKmPop"]) * sqKm_per_sqMi
     print ("Reformatting numbers...")
-    for col in dataCols:
+    for col in (dataCols + extraIntCols):
         df_interp[col] = df_interp[col].map(lambda x: reformat(x))
-    df_interp["SqMiles"] = df_interp["SqMiles"].map(lambda x: '{:.2f}'.format(x))
-    df_interp["SqKm"] = df_interp["SqKm"].map(lambda x: '{:.2f}'.format(x))
+    for col in extraFloatCols:
+        df_interp[col] = df_interp[col].map(lambda x:  '{:.2f}'.format(x))
     return df_interp
 
 def aggregate_dasymmetric(nlcd, df_dat, df_agg, dataCols, districtFIPSCol, stateFIPSCol='STATEFP', nJobs=-1):
@@ -130,24 +158,29 @@ def aggregate_dasymmetric(nlcd, df_dat, df_agg, dataCols, districtFIPSCol, state
     df_interp = tobler.dasymetric.masked_area_interpolate(raster=nlcd
                                                           , source_df=df_dat
                                                           , target_df=df_agg
-                                                          , extensive_variables=dataCols)
+                                                          , extensive_variables=(['TotalPopulation', 'TotalIncome','SqKmPop'] + dataCols))
     df_interp = pandas.concat([df_agg[[stateFIPSCol, districtFIPSCol] + ['SqMiles','SqKm']], df_interp],axis=1) # put the keys + areas back
     print("Removing ZZ entries")
     df_interp = df_interp[(df_interp[districtFIPSCol] != "ZZ")]
     df_interp = df_interp.rename(columns={stateFIPSCol: "StateFIPS", districtFIPSCol: "CongressionalDistrict"})
+    df_interp["PerCapitaIncome"] = df_interp["TotalIncome"]/df_interp["TotalPopulation"]
+    df_interp["PopPerSqMile"] = df_interp["TotalPopulation"]/df_interp["SqMiles"]
+    sqKm_per_sqMi = 2.58999
+    df_interp["pwPopPerSqMile"] = (df_interp["TotalPopulation"] * df_interp["TotalPopulation"]/df_interp["SqKmPop"]) * sqKm_per_sqMi
     print ("Reformatting numbers...")
-    for col in dataCols:
+    for col in (dataCols + extraIntCols):
         df_interp[col] = df_interp[col].map(lambda x: reformat(x))
-    df_interp["SqMiles"] = df_interp["SqMiles"].map(lambda x: '{:.2f}'.format(x))
-    df_interp["SqKm"] = df_interp["SqKm"].map(lambda x: '{:.2f}'.format(x))
+    for col in extraFloatCols:
+        df_interp[col] = df_interp[col].map(lambda x:  '{:.2f}'.format(x))
     return df_interp
 
 
-df_tracts, tract_dataCols = loadShapesAndData(dataCSVs, dataShapes)
+df_tracts, tract_dataCols = loadShapesAndData(dataCSVs, dataShapes, totalPopCol, pcIncomeCol)
 df_cds = loadAggregateToShapes(aggregateToShape)
 df_aggregated = aggregate_simple(df_tracts, df_cds, tract_dataCols, cdCol)
 #df_aggregated = aggregate_dasymmetric(nlcd, df_tracts, df_cds, tract_dataCols, cdCol)
-outCols = ['StateFIPS','CongressionalDistrict','SqMiles','SqKm'] + tract_dataCols
+
+outCols = ['StateFIPS','CongressionalDistrict'] + extraIntCols + extraFloatCols + tract_dataCols
 
 print ("Writing ", outCSV)
 df_aggregated[outCols].to_csv(outCSV, index=False)
